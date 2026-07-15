f2613a95df3a7d71a00b7d7c73a56caabcd1462a
nicolasniki202-coder <nicolasniki202@gmail.com>
2026-07-15 22:24:15 +0200

    Create maps.js

---
+++ utils/maps.js
@@ -0,0 +1,32 @@
+const maps = {
+  "de_mirage": {
+    name: "Mirage",
+    image: "https://raw.githubusercontent.com/nicolasniki202-coder/nexo-match-bot/main/assets/maps/mirage.jpg"
+  },
+  "de_inferno": {
+    name: "Inferno",
+    image: "https://raw.githubusercontent.com/nicolasniki202-coder/nexo-match-bot/main/assets/maps/inferno.jpg"
+  },
+  "de_dust2": {
+    name: "Dust II",
+    image: "https://raw.githubusercontent.com/nicolasniki202-coder/nexo-match-bot/main/assets/maps/dust2.jpg"
+  },
+  "de_nuke": {
+    name: "Nuke",
+    image: "https://raw.githubusercontent.com/nicolasniki202-coder/nexo-match-bot/main/assets/maps/nuke.jpg"
+  },
+  "de_ancient": {
+    name: "Ancient",
+    image: "https://raw.githubusercontent.com/nicolasniki202-coder/nexo-match-bot/main/assets/maps/ancient.jpg"
+  },
+  "de_anubis": {
+    name: "Anubis",
+    image: "https://raw.githubusercontent.com/nicolasniki202-coder/nexo-match-bot/main/assets/maps/anubis.jpg"
+  },
+  "de_train": {
+    name: "Train",
+    image: "https://raw.githubusercontent.com/nicolasniki202-coder/nexo-match-bot/main/assets/maps/train.jpg"
+  }
+};
+
+module.exports = maps;
